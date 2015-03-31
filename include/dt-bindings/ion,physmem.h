4f8b45e179d317ae7d162cb7c084563bbb1d3b6b
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2015-03-31 17:15:25 +0300

    ion-physmem: Add dt-bindings
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

---
+++ include/dt-bindings/ion,physmem.h
@@ -0,0 +1,17 @@
+/*
+ * This header provides constants for ION physmem driver.
+ *
+ */
+
+#ifndef _DT_BINDINGS_ION_PHYSMEM_H
+#define _DT_BINDINGS_ION_PHYSMEM_H
+
+#define ION_HEAP_TYPE_SYSTEM         0
+#define ION_HEAP_TYPE_SYSTEM_CONTIG  1
+#define	ION_HEAP_TYPE_CARVEOUT       2
+#define	ION_HEAP_TYPE_CHUNK          3
+#define	ION_HEAP_TYPE_DMA            4
+#define	ION_HEAP_TYPE_CUSTOM         5
+
+
+#endif
