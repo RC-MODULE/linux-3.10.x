e2d4de6ba1a21050ab2e7c8b533adf03be69ebf2
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2015-03-31 20:09:11 +0300

    clocksource: Fix coding style in arm_dit_timer.c
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- drivers/clocksource/arm_dit_timer.c
+++ drivers/clocksource/arm_dit_timer.c
@@ -1,7 +1,7 @@
 /*
- *  arch/arm/mach-uemd/clocksource.c
+ *  drivers/clocksource/arm_dit_timer.c
  *
- *  Copyright (C) 2011-2014  RC Module
+ *  Copyright (C) 2011-2015  RC Module
  *
  *  Sergey Mironov <ierton@gmail.com>
  *  Andrew Andrianov <andrew@ncrmnt.org>
@@ -54,13 +54,13 @@
 #define TIMER_CTRL_DISABLE     (0<<7)
 
 
-/* 
+/*
  * TIMER_CONTROL_PERIODIC:
  * The counter generates an interrupt at a constant interval, reloading the
  * original value after wrapping past zero.
  */
 #define TIMER_CTRL_PERIODIC    (1<<6)
-/* 
+/*
  * TIMER_CONTROL_FREERUNNING:
  * The counter wraps after reaching its zero value, and continues to count down
  * from the maximum value. This is the default mode.
@@ -78,7 +78,7 @@
 /* Counter register size is 32 Bit long */
 #define TIMER_CTRL_32BIT           (1<<1)
 
-/* 
+/*
  * TIMER_CONTROL_ONESHOT:
  * The counter generates an interrupt once. When the counter reaches zero, it
  * halts until reprogrammed by the user. This can be achieved by either clearing
@@ -96,7 +96,7 @@ struct dit_timer {
 	void __iomem *timer_ctrl;
 	void __iomem *timer_load;
 	void __iomem *timer_iclr;
-	u32           freq; 
+	u32           freq;
 	struct clock_event_device evt;
 	struct irqaction act;
 };
@@ -108,19 +108,20 @@ static u64 notrace dit_sched_read(void)
 	return ~readl_relaxed(system_clock);
 }
 
-static int dit_next_event(unsigned long cycles, struct clock_event_device *evt_dev)
+static int dit_next_event(unsigned long cycles,
+			  struct clock_event_device *evt_dev)
 {
 	struct dit_timer *timer = container_of(evt_dev,
 		struct dit_timer, evt);
-
 	unsigned long ctrl = readl(timer->timer_ctrl);
+
 	writel_relaxed(cycles, timer->timer_load);
 	writel_relaxed(ctrl | TIMER_CTRL_ENABLE, timer->timer_ctrl);
-
 	return 0;
 }
 
-static void dit_set_mode(enum clock_event_mode mode, struct clock_event_device *evt_dev)
+static void dit_set_mode(enum clock_event_mode mode,
+			 struct clock_event_device *evt_dev)
 {
 	struct dit_timer *timer = container_of(evt_dev,
 		struct dit_timer, evt);
@@ -128,7 +129,7 @@ static void dit_set_mode(enum clock_event_mode mode, struct clock_event_device *
 
 	switch (mode) {
 	case CLOCK_EVT_MODE_PERIODIC:
-		writel_relaxed((timer->freq / HZ) -1, timer->timer_load);
+		writel_relaxed((timer->freq / HZ) - 1, timer->timer_load);
 		ctrl = TIMER_CTRL_PERIODIC;
 		ctrl |= TIMER_CTRL_32BIT | TIMER_CTRL_IE | TIMER_CTRL_ENABLE;
 		break;
@@ -150,26 +151,24 @@ static void dit_set_mode(enum clock_event_mode mode, struct clock_event_device *
 
 static irqreturn_t dit_interrupt(int irq, void *dev_id)
 {
-	struct dit_timer *timer = dev_id; 
+	struct dit_timer *timer = dev_id;
+
 	writel(1, timer->timer_iclr);
 	timer->evt.event_handler(&timer->evt);
 	return IRQ_HANDLED;
 }
-
-
-
 
 static void __init dit_time_init(struct device_node *node)
 {
 	struct dit_timer *timer;
 	void __iomem     *base;
 	u32               freq, mode;
-	int               irq; 
+	int               irq;
 
 	/* Just in case */
 	BUG_ON(system_clock);
-	
-	/*  Timer 1 is used as a clocksource */	
+
+	/*  Timer 1 is used as a clocksource */
 	base = of_iomap(node, 0);
 	if (!base)
 		panic("DIT: Can't remap registers\n");
@@ -181,17 +180,17 @@ static void __init dit_time_init(struct device_node *node)
 	sched_clock_register(dit_sched_read, 32, freq);
 	clocksource_mmio_init(base + TIMER1(TIMER_VALUE), node->name,
 		freq, 300, 32, clocksource_mmio_readl_down);
-	
+
 	/* Fire it now ! */
 	writel_relaxed(0,           base + TIMER1(TIMER_CONTROL));
 	writel_relaxed(0xffffffff,  base + TIMER1(TIMER_LOAD));
 	writel_relaxed(0xffffffff,  base + TIMER1(TIMER_VALUE));
 
-	mode = 	TIMER_CTRL_ENABLE 		|
-		TIMER_CTRL_32BIT 		| 
-		TIMER_CTRL_PERIODIC 		|
-		TIMER_CTRL_P1 			|
-		TIMER_CTRL_IDIS 		|
+	mode =  TIMER_CTRL_ENABLE	|
+		TIMER_CTRL_32BIT	|
+		TIMER_CTRL_PERIODIC	|
+		TIMER_CTRL_P1		|
+		TIMER_CTRL_IDIS		|
 		0;
 	writel_relaxed(mode, base + TIMER1(TIMER_CONTROL));
 
@@ -199,8 +198,6 @@ static void __init dit_time_init(struct device_node *node)
 	irq = irq_of_parse_and_map(node, 0);
 	if (irq <= 0)
 		panic("DIT: Can't parse IRQ\n");
-	
-	printk(KERN_INFO "irq %d\n", irq);
 
 	timer = kzalloc(sizeof(*timer), GFP_KERNEL);
 	if (!timer)
@@ -208,7 +205,7 @@ static void __init dit_time_init(struct device_node *node)
 
 	/* Good, now let's fill our struct with sensible data */
 	timer->base = base;
-	timer->freq = freq; 
+	timer->freq = freq;
 
 	timer->timer_ctrl   = base + TIMER0(TIMER_CONTROL);
 	timer->timer_load   = base + TIMER0(TIMER_LOAD);
@@ -216,7 +213,8 @@ static void __init dit_time_init(struct device_node *node)
 
 	timer->evt.name           = node->name;
 	timer->evt.rating         = 300;
-	timer->evt.features       = CLOCK_EVT_FEAT_ONESHOT | CLOCK_EVT_FEAT_PERIODIC;
+	timer->evt.features       =	CLOCK_EVT_FEAT_ONESHOT |
+					CLOCK_EVT_FEAT_PERIODIC;
 	timer->evt.set_mode       = dit_set_mode;
 	timer->evt.set_next_event = dit_next_event;
 	timer->evt.cpumask        = cpumask_of(0);
@@ -225,14 +223,11 @@ static void __init dit_time_init(struct device_node *node)
 	timer->act.flags   = IRQF_TIMER | IRQF_SHARED;
 	timer->act.dev_id  = timer;
 	timer->act.handler = dit_interrupt;
-	
+
 	if (setup_irq(irq, &timer->act))
 		panic("DIT: Can't set up timer IRQ\n");
 
 	clockevents_config_and_register(&timer->evt, freq, 0xf, 0xffffffff);
-
-	printk("DIT: Timer system initialized\n");
-
 }
 
 CLOCKSOURCE_OF_DECLARE(arm_dit, "arm,arm-dit-timer",
