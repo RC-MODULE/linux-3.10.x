f4cf202f30d94389524613bb073729241eb71e66
Andrew Andrianov <andrew@ncrmnt.org>
2016-08-08 15:01:58 +0300

    uemd: Make sure wdt is unlocked when rebooting
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- arch/arm/mach-uemd/uemd.c
+++ arch/arm/mach-uemd/uemd.c
@@ -179,6 +179,7 @@ void uemd_pm_restart(char mode, const char *cmd)
 	void __iomem *regs;
 	pr_info("platform: Requested system restart\n");
 	regs = ioremap_nocache(0x20025000, 0xfff);
+	iowrite32(0x1ACCE551, (regs + 0xc00));
 	iowrite32(1, (regs + 0xf00));
 	iowrite32(1, (regs + 0xf04));
 	/* Goodbye! */
