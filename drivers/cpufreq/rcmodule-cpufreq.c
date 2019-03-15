f8c2df5062cd0693bfc66483e837f82cb5eb2757
Alexey Spirkov <alexeis@astrosoft.ru>
2019-03-15 19:43:36 +0300

    CPUfreq driver

--- drivers/cpufreq/rcmodule-cpufreq.c
+++ drivers/cpufreq/rcmodule-cpufreq.c
@@ -23,51 +23,86 @@
 #include <linux/time.h>
 #include <linux/of_device.h>
 #include <linux/io.h>
+#include <linux/mfd/syscon.h>
+#include <linux/regmap.h>
 
 
 #define DBG(fmt...) pr_debug(fmt)
 
 #define CPUFREQ_PLLSTATE_OFFSET 0x000
 #define CPUFREQ_PLLCTRL_OFFSET 0x004
-#define CPUFREQ_PLLDIV_OFFSET 0x008
+#define CPUFREQ_PRDIV_OFFSET 0x0030
+#define CPUFREQ_FBDIV_OFFSET 0x0034
+#define CPUFREQ_PSDIV_OFFSET 0x0038
+#define CPUFREQ_CKDIVMODE_CPU_OFFSET 0x0100
 #define CPUFREQ_WRLOCK_OFFSET 0x00C
 #define CPUFREQ_WRUNLOCK 0x1ACCE551
 
 
-static struct cpufreq_frequency_table rcmodule_cpu_freqs[] = {
-	{0, 0x03010050, 250000},
-	{0, 0x02010050, 500000},
-	{0, 0x0101003C, 750000},
-//	{0, 0x01010048, 900000},		// todo enable on actual HW and extend with required values
-//	{0, 0x01010050, 1000000},
-	{0, 0, CPUFREQ_TABLE_END},
+struct freq_mode {
+	unsigned int freq_khz;
+	unsigned int fbdiv; 	// 8..511
+	unsigned int prediv; 	// 1..31
+	unsigned int postdiv;   // 2'b00 - 1; 2'b01 - 2; 2'b10 - 4; 2'b11 - 8
+	unsigned int cpudiv;    // 0 - 255; Fclk = Fpll/(cpudiv+1)
 };
 
-
 struct rcmodule_cpufreq_data {
-	void __iomem *regs;
+    struct regmap *control;
+    struct cpufreq_frequency_table *cpu_freqs;
+    struct freq_mode *cpu_modes;
 	int curr_mode;
 };
 
-static int rcmodule_switch_cpufreq(struct rcmodule_cpufreq_data* drv_data, unsigned int freqkHz, unsigned int hwvalue)
+static int rcmodule_switch_cpufreq(struct rcmodule_cpufreq_data* drv_data, unsigned int index)
 {
 	unsigned long flags;
+    unsigned int val;
 
 	local_irq_save(flags);
 
-	writel(CPUFREQ_WRUNLOCK, drv_data->regs + CPUFREQ_WRLOCK_OFFSET);
-	writel(hwvalue, drv_data->regs + CPUFREQ_PLLDIV_OFFSET);
-	writel(0x1, drv_data->regs + CPUFREQ_PLLCTRL_OFFSET);
+    if(! regmap_write(drv_data->control, CPUFREQ_WRLOCK_OFFSET, CPUFREQ_WRUNLOCK)) // unlock
+    {
 
-	// wait while PLL will be stabilized enough 
-	// just estimated behavior - no manual for a while
-	while(!(readl(drv_data->regs + CPUFREQ_PLLSTATE_OFFSET) & 0x1));
+        //printk("write %08X to FBDIV\n", drv_data->cpu_modes[index].fbdiv);
+        //printk("write %08X to PRDIV\n", drv_data->cpu_modes[index].prediv);
+        //printk("write %08X to PSDIV\n", drv_data->cpu_modes[index].postdiv);
+        //printk("write %08X to CKDIVMODE\n", drv_data->cpu_modes[index].cpudiv);
 
-	local_irq_restore(flags);
-	ppc_proc_freq = freqkHz*1000;
+        // write new data to PLL control
+        regmap_write(drv_data->control, CPUFREQ_FBDIV_OFFSET, drv_data->cpu_modes[index].fbdiv);
+        regmap_write(drv_data->control, CPUFREQ_PRDIV_OFFSET, drv_data->cpu_modes[index].prediv); 
+        regmap_write(drv_data->control, CPUFREQ_PSDIV_OFFSET, drv_data->cpu_modes[index].postdiv); 
+        regmap_write(drv_data->control, CPUFREQ_CKDIVMODE_CPU_OFFSET, drv_data->cpu_modes[index].cpudiv); 
+        
+        regmap_update_bits(drv_data->control, CPUFREQ_PLLCTRL_OFFSET, BIT(0), 1); // restart PLL
 
-	return 0;
+        regmap_write(drv_data->control, CPUFREQ_WRLOCK_OFFSET, 0);  // lock back
+
+        // wait while PLL will be stabilized enough 
+        // just estimated behavior - no manual for a while
+        do
+        {
+            if(regmap_read(drv_data->control, CPUFREQ_PLLSTATE_OFFSET, &val))
+            {
+                pr_err("Unable to access control regmap\n");
+                local_irq_restore(flags);
+                return 2;
+            }
+        } while(!(val & 0x1));
+
+        ppc_proc_freq = drv_data->cpu_modes[index].freq_khz*1000;
+        local_irq_restore(flags);
+    	return 0;
+    }
+    else
+    {
+        pr_err("Unable to access control regmap\n");
+        local_irq_restore(flags);
+        return 1;
+    }
 }
+
 static struct cpufreq_driver rcmodule_cpufreq_driver;
 
 static int rcmodule_cpufreq_target(struct cpufreq_policy *policy,
@@ -75,7 +110,7 @@ static int rcmodule_cpufreq_target(struct cpufreq_policy *policy,
 {
 	int res;
 	struct rcmodule_cpufreq_data* data = rcmodule_cpufreq_driver.driver_data;
-	res = rcmodule_switch_cpufreq(data, rcmodule_cpu_freqs[index].frequency, rcmodule_cpu_freqs[index].driver_data);
+	res = rcmodule_switch_cpufreq(data, data->cpu_freqs[index].driver_data);
 	if(!res)
 		// just for get function...
 		data->curr_mode = index;
@@ -84,13 +119,14 @@ static int rcmodule_cpufreq_target(struct cpufreq_policy *policy,
 
 static int rcmodule_cpufreq_init(struct cpufreq_policy *policy)
 {
-	return cpufreq_generic_init(policy, rcmodule_cpu_freqs, 12000);		// to check latency
+	struct rcmodule_cpufreq_data* data = rcmodule_cpufreq_driver.driver_data;
+	return cpufreq_generic_init(policy, data->cpu_freqs, 12000);		// to check latency
 }
 
 static unsigned int rcmodule_get_cpu_freq(unsigned int cpu)
 {
 	struct rcmodule_cpufreq_data* data = rcmodule_cpufreq_driver.driver_data;
-	return rcmodule_cpu_freqs[data->curr_mode].frequency;
+	return data->cpu_modes[data->curr_mode].freq_khz;
 }
 
 static struct cpufreq_driver rcmodule_cpufreq_driver = {
@@ -107,23 +143,54 @@ static struct cpufreq_driver rcmodule_cpufreq_driver = {
 static int rcmodule_cpufreq_probe(struct platform_device *pdev)
 {
 	struct rcmodule_cpufreq_data *data;
-	struct resource *res;
-	struct device_node *cpunode;
+	struct device_node *cpunode, *tmp;
 	int rc = -ENODEV, index, i;
 	unsigned int min, max;
 	const u32 *valp;
 	struct cpufreq_frequency_table* table;
+	struct property *prop;
+    int sz, count;
 
 	data = devm_kzalloc(&pdev->dev, sizeof(*data), GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	data->regs = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(data->regs)) {
-		pr_err("Can't find registers information for CPU frequency driver\n");
-		goto err0;
+    prop = of_find_property(pdev->dev.of_node, "freqs", &sz);
+
+	if (!prop)
+    {
+		pr_err("failed to find frequency table\n");
+		goto err0;					
+    }
+    count = sz / sizeof(struct freq_mode);
+
+    data->cpu_modes = devm_kzalloc(&pdev->dev, sz, GFP_KERNEL);
+    data->cpu_freqs = devm_kzalloc(&pdev->dev, (count + 1) * sizeof(struct cpufreq_frequency_table), GFP_KERNEL);
+
+    if( of_property_read_u32_array(pdev->dev.of_node, "freqs",
+                        (u32 *)data->cpu_modes, sz/sizeof(unsigned int)))
+    {
+		pr_err("failed to read frequency table\n");
+		goto err0;					        
+    }
+
+    for(i = 0; i < count; i++)
+    {
+        data->cpu_freqs[i].flags = 0;
+        data->cpu_freqs[i].driver_data = i;
+        data->cpu_freqs[i].frequency = data->cpu_modes[i].freq_khz;
+    }
+    data->cpu_freqs[i].flags = 0;
+    data->cpu_freqs[i].driver_data = 0;
+    data->cpu_freqs[i].frequency = CPUFREQ_TABLE_END;
+
+	tmp = of_parse_phandle(pdev->dev.of_node, "control", 0);
+	if (!tmp) {
+		pr_err("failed to find control register reference\n");
+		goto err0;					
 	}
+	data->control = syscon_node_to_regmap(tmp);
+
 
 	/* Get first CPU node */
 	cpunode = of_cpu_device_node_get(0);
@@ -143,7 +210,7 @@ static int rcmodule_cpufreq_probe(struct platform_device *pdev)
 	min = 0xffffffff;
 	max = 0;
 	// find desired index
-	for(i = 0, table = rcmodule_cpu_freqs; table->frequency != CPUFREQ_TABLE_END; table++, i++)
+	for(i = 0, table = data->cpu_freqs; table->frequency != CPUFREQ_TABLE_END; table++, i++)
 	{
 		if(table->frequency == (*valp)/1000)
 			index = i;
@@ -161,9 +228,9 @@ static int rcmodule_cpufreq_probe(struct platform_device *pdev)
 	pr_info("Low: %d Mhz, High: %d Mhz, Cur: %d MHz\n",
 		min/1000,
 		max/1000,
-		rcmodule_cpu_freqs[index].frequency/1000);
+		data->cpu_freqs[index].frequency/1000);
 
-	rcmodule_switch_cpufreq(data, rcmodule_cpu_freqs[index].frequency, rcmodule_cpu_freqs[index].driver_data);
+	rcmodule_switch_cpufreq(data, data->cpu_freqs[index].driver_data);
 	data->curr_mode = index;
 
 	rcmodule_cpufreq_driver.driver_data = data;
@@ -175,8 +242,6 @@ static int rcmodule_cpufreq_probe(struct platform_device *pdev)
 	return rc;
 
 err1:
-	devm_iounmap(&pdev->dev, data->regs);
-
 err0:
 	of_node_put(cpunode);
 
@@ -185,9 +250,7 @@ err0:
 
 static int rcmodule_cpufreq_remove(struct platform_device *pdev)
 {
-	struct rcmodule_cpufreq_data *data = platform_get_drvdata(pdev);
 	cpufreq_unregister_driver(&rcmodule_cpufreq_driver);
-	devm_iounmap(&pdev->dev, data->regs);
 	return 0;
 }
 
