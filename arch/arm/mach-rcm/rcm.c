7bdf93dabac93457122d20c66e05f0e76d96d058
Alexander Shtreys <strav@list.ru>
2020-05-07 14:39:59 +0300

    Workaround for WFI-problem. Purpose of fix is to avoid of calling cpu_v7_do_idle().

--- arch/arm/mach-rcm/rcm.c
+++ arch/arm/mach-rcm/rcm.c
@@ -10,23 +10,39 @@
 #include <linux/of_platform.h>
 #include <linux/clocksource.h>
 #include <asm/mach/arch.h>
+#include <asm/system_misc.h> 
+
+static void rcm_pm_idle(void)
+{
+	asm volatile (
+		"nop\n\t"
+		"nop\n\t");
+}
 
 static void __init rcm_dt_timer_init(void)
 {
-    of_clk_init(NULL);
-    timer_probe();
-}   
+	of_clk_init(NULL);
+	timer_probe();
+}
 
 static const char *rcm_1879vm8ya_dt_match[] __initconst = {
-    "rcm,1879VM8YA",
-    NULL
+	"rcm,1879VM8YA",
+	NULL
 };
 
+static int __init rcm_init(void)
+{
+	arm_pm_idle = rcm_pm_idle;
+	return 0;
+}
+
+arch_initcall(rcm_init); 
+
 DT_MACHINE_START(RCM_1879VM8YA, "RCM 1879VM8YA Module")
-    //.init_time      = timer_probe,
-    .init_time      = rcm_dt_timer_init,
-    .l2c_aux_mask   = 0xffffffff,
-    .l2c_aux_val    =  0x00000000,
-    .dt_compat      = rcm_1879vm8ya_dt_match,
+	//.init_time      = timer_probe,
+	.init_time      = rcm_dt_timer_init,
+	.l2c_aux_mask   = 0xffffffff,
+	.l2c_aux_val    =  0x00000000,
+	.dt_compat      = rcm_1879vm8ya_dt_match,
 MACHINE_END
 
