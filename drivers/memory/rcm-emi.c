52d8c0c623a9232c35cbb30c080186bb17b3c314
Mikhail Petrov <Mikhail.Petrov@astrosoft.ru>
2020-08-13 10:10:06 +0300

    [bm18-emi] version with ECC (no NOR)

--- drivers/memory/rcm-emi.c
+++ drivers/memory/rcm-emi.c
@@ -11,7 +11,9 @@
 #include <linux/module.h>
 #include <linux/cpufreq.h>
 #include <linux/of_device.h>
+#include <linux/interrupt.h>
 #include <asm/dcr.h>
+#include <dt-bindings/memory/rcm-emi.h>
 
 #define BANK_COUNT 6
 
@@ -58,6 +60,8 @@ struct bank_config
 	u32 ss;
 	u32 sd;
 	bool ecc;
+	u32 base;
+	u32 size;
 };
 
 struct emi_config
@@ -72,6 +76,18 @@ struct emi_data
 	dcr_host_t dcr_host;
 	unsigned dcr_n;
 	unsigned dcr_c;
+	bool ecc_irq_err_single_allocated;
+	int ecc_irq_err_single;
+	bool ecc_irq_err_double_allocated;
+	int ecc_irq_err_double;
+};
+
+static const u32 BANK_BASE_ADDRESSES[BANK_COUNT] = {
+	0x00000000, 0x20000000, 0x40000000, 0x50000000, 0x60000000, 0x70000000
+};
+
+static const u32 BANK_DEFAULT_SIZES[BANK_COUNT] = {
+	0x20000000, 0x20000000, 0x10000000, 0x10000000, 0x10000000, 0x10000000
 };
 
 static int read_config(struct platform_device *pdev)
@@ -97,7 +113,76 @@ static int read_config(struct platform_device *pdev)
 		sprintf(prop_name, "bank-%i-ecc", i);
 		emi->config.banks[i].ecc = of_property_read_bool(pdev->dev.of_node, prop_name);
 
+		emi->config.banks[i].base = BANK_BASE_ADDRESSES[i];
+
+		sprintf(prop_name, "bank-%i-size", i);
+		ret = of_property_read_u32(pdev->dev.of_node, prop_name, &emi->config.banks[i].size);
+		if (ret != 0)
+			return ret;
+
 		emi->config.banks[i].configured = true;
+	}
+
+	return 0;
+}
+
+static irqreturn_t ecc_err_single_interrupt(int irq, void *dev_id)
+{
+	struct platform_device *pdev = (struct platform_device*)dev_id;
+	struct emi_data *emi = dev_get_drvdata(&pdev->dev);
+	u32 irr = dcr_read(emi->dcr_host, RCM_EMI_IRR);
+	u32 h1adr = dcr_read(emi->dcr_host, RCM_EMI_H1ADR);
+
+	dev_err(&pdev->dev, "single ECC error: EMI_IRR = 0x%08X, EMI_H1ADR = 0x%08X\n",
+		irr, h1adr);
+
+	// clear the interrupt and the error address
+	dcr_write(emi->dcr_host, RCM_EMI_H1CMR, 0);
+	dcr_write(emi->dcr_host, RCM_EMI_IRR_RST, (irr & 0x5A5));
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t ecc_err_double_interrupt(int irq, void *dev_id)
+{
+	struct platform_device *pdev = (struct platform_device*)dev_id;
+	struct emi_data *emi = dev_get_drvdata(&pdev->dev);
+	u32 irr = dcr_read(emi->dcr_host, RCM_EMI_IRR);
+	u32 h2adr = dcr_read(emi->dcr_host, RCM_EMI_H2ADR);
+
+	dev_err(&pdev->dev, "double ECC error: EMI_IRR = 0x%08X, EMI_H2ADR = 0x%08X\n",
+		irr, h2adr);
+
+	dcr_write(emi->dcr_host, RCM_EMI_IMR, 0); // mask interrupts
+	panic("Memory double ECC error\n");
+
+	return IRQ_HANDLED;
+}
+
+static int fill_for_ecc(struct platform_device *pdev, struct bank_config *bank)
+{
+	u32 base;
+	u32 size;
+	u32 page_size;
+	void *p;
+
+	dev_info(&pdev->dev, "Filling memory region 0x%08x-0x%08x for ECC\n",
+		bank->base, bank->base + bank->size);
+
+	base = bank->base;
+	size = bank->size;
+
+	while (size != 0) {
+		page_size = min_t(u32, size, PAGE_SIZE);
+
+		p = devm_memremap(&pdev->dev, base, PAGE_SIZE, MEMREMAP_WC);
+		if (IS_ERR(p))
+			return PTR_ERR(p);
+		memset(p, 0, page_size);
+		devm_memunmap(&pdev->dev, p);
+
+		base += page_size;
+		size -= page_size;
 	}
 
 	return 0;
@@ -106,16 +191,19 @@ static int read_config(struct platform_device *pdev)
 static int setup_hardware(struct platform_device *pdev)
 {
 	struct emi_data *emi = dev_get_drvdata(&pdev->dev);
-	int i;
+	struct bank_config *bank;
 	u32 hstsr;
+	int i;
+	int ret;
 
-	dev_dbg(&pdev->dev, "bank conf     ss         sd\n");
-
+	dcr_write(emi->dcr_host, RCM_EMI_IMR, 0); // mask interrupts
 	hstsr = dcr_read(emi->dcr_host, RCM_EMI_HSTSR);
+
+	dev_dbg(&pdev->dev, "bank cfg    ss         sd    ecc   base       size\n");
 
 	for (i = 0; i < BANK_COUNT; ++i)
 	{
-		struct bank_config *bank = &emi->config.banks[i];
+		bank = &emi->config.banks[i];
 
 		if (bank->configured) {
 			dcr_write(emi->dcr_host, RCM_EMI_SS0 + (RCM_EMI_SS1 - RCM_EMI_SS0) * i, bank->ss);
@@ -128,15 +216,40 @@ static int setup_hardware(struct platform_device *pdev)
 			bank->ss = dcr_read(emi->dcr_host, RCM_EMI_SS0 + (RCM_EMI_SS1 - RCM_EMI_SS0) * i);
 			bank->sd = dcr_read(emi->dcr_host, RCM_EMI_SD0 + (RCM_EMI_SD1 - RCM_EMI_SD0) * i);
 			bank->ecc = ((hstsr & (1 << i)) != 0);
+			bank->base = BANK_BASE_ADDRESSES[i];
+			bank->size = BANK_DEFAULT_SIZES[i];
 		}
 
-		dev_dbg(&pdev->dev, " %i    %i   0x%08x 0x%08x\n",
-			i, (int)bank->configured, bank->ss, bank->sd);
+		dev_dbg(&pdev->dev, " %i    %i 0x%08X 0x%08X %i 0x%08X 0x%08X\n",
+			i, (int)bank->configured, bank->ss, bank->sd, (int)bank->ecc, bank->base, bank->size);
 	}
 
 	dcr_write(emi->dcr_host, RCM_EMI_HSTSR, hstsr);
 
+	// clear state and unmask interrupts
+	dcr_write(emi->dcr_host, RCM_EMI_H1CMR, 0);
+	dcr_write(emi->dcr_host, RCM_EMI_H2CMR, 0);
+	dcr_write(emi->dcr_host, RCM_EMI_IRR_RST, 0xFFFFFFFF);
+	dcr_write(emi->dcr_host, RCM_EMI_IMR, 0xFFF);
+
 	mb();
+
+	// clear banks with ECC
+	for (i = 0; i < BANK_COUNT; ++i)
+	{
+		bank = &emi->config.banks[i];
+		if (bank->configured && bank->ecc) {
+			switch (bank->ss & RCM_EMI_SSx_BTYP_MASK) {
+			case RCM_EMI_SSx_BTYP_SRAM:
+			case RCM_EMI_SSx_BTYP_SSRAM:
+			case RCM_EMI_SSx_BTYP_SDRAM:
+				ret = fill_for_ecc(pdev, bank);
+				if (ret != 0)
+					return ret;
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
@@ -171,6 +284,26 @@ static int probe_internal(struct platform_device *pdev)
 		return ret;
 	}
 
+	emi->ecc_irq_err_single = platform_get_irq(pdev, 0);
+	if (emi->ecc_irq_err_single <= 0)
+		return emi->ecc_irq_err_single;
+	ret = request_irq(emi->ecc_irq_err_single, ecc_err_single_interrupt, 0, "emi", pdev);
+	if (ret != 0) {
+		dev_err(&pdev->dev, "could not allocate interrupt for single ECC errors\n");
+		return ret;
+	}
+	emi->ecc_irq_err_single_allocated = true;
+
+	emi->ecc_irq_err_double = platform_get_irq(pdev, 1);
+	if (emi->ecc_irq_err_double <= 0)
+		return emi->ecc_irq_err_double;
+	ret = request_irq(emi->ecc_irq_err_double, ecc_err_double_interrupt, 0, "emi", pdev);
+	if (ret != 0) {
+		dev_err(&pdev->dev, "could not allocate interrupt for double ECC errors\n");
+		return ret;
+	}
+	emi->ecc_irq_err_double_allocated = true;
+
 	ret = setup_hardware(pdev);
 	if (ret != 0) {
 		dev_err(&pdev->dev, "failed to setup hardware\n");
@@ -186,6 +319,12 @@ static void cleanup(struct platform_device *pdev)
 
 	if (emi == NULL)
 		return;
+
+	if (emi->ecc_irq_err_single_allocated)
+		free_irq(emi->ecc_irq_err_single, pdev);
+
+	if (emi->ecc_irq_err_double_allocated)
+		free_irq(emi->ecc_irq_err_double, pdev);
 
 	if (emi->dcr_host_mapped)
 		dcr_unmap(emi->dcr_host, emi->dcr_c);
