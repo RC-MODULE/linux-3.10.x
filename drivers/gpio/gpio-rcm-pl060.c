b1fe060a9c07486c7f922cfc4293a051bfcbe565
Alexander Shtreys <alexander.shtreys@mir.dev>
2020-09-25 17:21:32 +0300

    [gpio-rcm-pl060] Fixes after real testing/debug

--- drivers/gpio/gpio-rcm-pl060.c
+++ drivers/gpio/gpio-rcm-pl060.c
@@ -28,9 +28,8 @@
 struct rcm_pl060_chip {
 	struct gpio_chip   chip;
 	struct regmap     *reg;
-//	struct irq_domain *domain;
-	struct irq_chip    intc;
-	int               *irqs;
+	struct irq_domain *domain;
+	int                cnt_irqs;
 };
 
 static const struct regmap_config rcm_pl060_regmap_config = {
@@ -44,10 +43,10 @@ static const struct of_device_id rcm_pl060_of_match[];
 
 static int rcm_pl060_check_irq(struct rcm_pl060_chip *data, unsigned long hwirq)
 {
-	if (hwirq >= data->chip.irq.num_parents) {
+	if (hwirq >= data->cnt_irqs) {
 		pr_err("%s: Incorrect child hardware IRQ offset (%lu). "
 		       "Possible offsets are [0, %u]\n", __func__, 
-		       hwirq, data->chip.irq.num_parents - 1);
+		       hwirq, data->cnt_irqs - 1);
 		return 0;
 	}
 
@@ -118,14 +117,7 @@ static int rcm_pl060_direction_output(struct gpio_chip *chip, unsigned offset,
 
 	return 0;
 }
-/*
-static int rcm_pl060_to_irq(struct gpio_chip *chip, unsigned int offset)
-{
-	struct rcm_pl060_chip *data = gpiochip_get_data(chip);
 
-	return irq_create_mapping(data->domain, offset);
-}
-*/
 static void rcm_pl060_set_regime(struct rcm_pl060_chip *data, 
                                  unsigned r0, unsigned r1,
                                  unsigned r2, unsigned r3)
@@ -134,61 +126,51 @@ static void rcm_pl060_set_regime(struct rcm_pl060_chip *data,
 	               ((r2 & 0x3) << 4) | ((r3 & 0x3) << 6);
 
 	regmap_update_bits(data->reg, RCM_PL060_REGIME,
-	                   0x0F, val);
+	                   0xFF, val);
 }
 
 static void rcm_pl060_irq_ack(struct irq_data *d)
 {
-	struct rcm_pl060_chip *data = irq_data_get_irq_chip_data(d);
-
-	pr_debug("%s(%ld) >>>\n", __func__, d->hwirq);
+	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
+	struct rcm_pl060_chip *data = gc->private;
 
 	if (!rcm_pl060_check_irq(data, d->hwirq))
 		return;
 
 	regmap_update_bits(data->reg, RCM_PL060_EIREQ,
 	                   BIT(d->hwirq), 0);
-
-	pr_debug("%s <<<\n", __func__);
 }
 
 static void rcm_pl060_irq_mask(struct irq_data *d)
 {
-	struct rcm_pl060_chip *data = irq_data_get_irq_chip_data(d);
-
-	pr_debug("%s(%ld) >>>\n", __func__, d->hwirq);
-
-	if (!rcm_pl060_check_irq(data, d->hwirq))
-		return;
-
-	regmap_update_bits(data->reg, RCM_PL060_EIENB,
-	                   BIT(d->hwirq), BIT(d->hwirq));
-
-	pr_debug("%s <<<\n", __func__);
-}
-
-static void rcm_pl060_irq_unmask(struct irq_data *d)
-{
-	struct rcm_pl060_chip *data = irq_data_get_irq_chip_data(d);
-
-	pr_debug("%s(%ld) >>>\n", __func__, d->hwirq);
+	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
+	struct rcm_pl060_chip *data = gc->private;
 
 	if (!rcm_pl060_check_irq(data, d->hwirq))
 		return;
 
 	regmap_update_bits(data->reg, RCM_PL060_EIENB,
 	                   BIT(d->hwirq), 0);
+}
 
-	pr_debug("%s <<<\n", __func__);
+static void rcm_pl060_irq_unmask(struct irq_data *d)
+{
+	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
+	struct rcm_pl060_chip *data = gc->private;
+
+	if (!rcm_pl060_check_irq(data, d->hwirq))
+		return;
+
+	regmap_update_bits(data->reg, RCM_PL060_EIENB,
+	                   BIT(d->hwirq), BIT(d->hwirq));
 }
 
 static int rcm_pl060_irq_set_type(struct irq_data *d, unsigned int flow_type)
 {
-	struct rcm_pl060_chip *data = irq_data_get_irq_chip_data(d);
+	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
+	struct rcm_pl060_chip *data = gc->private;
 	unsigned val = 0;
 	unsigned type_parent;
-
-	pr_debug("%s(%ld, %u) >>>\n", __func__, d->hwirq, flow_type);
 
 	if (!rcm_pl060_check_irq(data, d->hwirq))
 		return -EINVAL;
@@ -222,43 +204,7 @@ static int rcm_pl060_irq_set_type(struct irq_data *d, unsigned int flow_type)
 	type_parent = (flow_type & IRQ_TYPE_EDGE_BOTH) ? IRQ_TYPE_EDGE_RISING : 
 	                                                 IRQ_TYPE_LEVEL_HIGH;
 
-	return irq_chip_set_type_parent(d, type_parent);
-}
-
-static int rcm_pl060_child_to_parent_hwirq(struct gpio_chip *chip,
-                                           unsigned int child_hwirq,
-                                           unsigned int child_type,
-                                           unsigned int *parent_hwirq,
-                                           unsigned int *parent_type)
-{
-	struct rcm_pl060_chip *data = gpiochip_get_data(chip);
-	struct irq_data *irq_data;
-
-	pr_debug("%s >>>\n", __func__);
-
-	if (!rcm_pl060_check_irq(data, child_hwirq))
-		return -EINVAL;
-
-	irq_data = irq_get_irq_data(chip->irq.parents[child_hwirq]);
-
-	*parent_hwirq = irq_data->hwirq - 32;
-	*parent_type = child_type;
-
-	pr_debug("%s: child_hwirq = %u, child_type = %u, parent_hwirq = %u\n", 
-	         __func__, child_hwirq, child_type, *parent_hwirq);
-
 	return 0;
-}
-
-static void rcm_pl060_populate_parent_fwspec(struct gpio_chip *chip,
-                                             struct irq_fwspec *fwspec,
-                                             unsigned int parent_hwirq,
-                                             unsigned int parent_type)
-{
-	fwspec->param_count = 3;
-	fwspec->param[0] = 0;
-	fwspec->param[1] = parent_hwirq;
-	fwspec->param[2] = parent_type;
 }
 
 static void rcm_pl060_irq_handler(struct irq_desc *desc)
@@ -268,18 +214,14 @@ static void rcm_pl060_irq_handler(struct irq_desc *desc)
 	unsigned req;
 	unsigned i;
 
-	pr_debug("%s(%ld) >>>\n", __func__, desc->irq_data.hwirq);
-
 	chained_irq_enter(chip, desc);
 
 	regmap_read(data->reg, RCM_PL060_EIREQ, &req);
 
-	pr_debug("%s: REQ = 0x%02X\n", __func__, req);
-
-	for (i = 0; i < RCM_PL060_GPIO_NR; i++)
+	for (i = 0; i < data->cnt_irqs; i++)
 		if (req & BIT(i)) {
 			unsigned irq = 
-				irq_find_mapping(data->chip.irq.domain, i);
+				irq_find_mapping(data->domain, i);
 
 			if (WARN_ON(irq == 0))
 				continue;
@@ -288,8 +230,6 @@ static void rcm_pl060_irq_handler(struct irq_desc *desc)
 		}
 
 	chained_irq_exit(chip, desc);
-
-	pr_debug("%s <<<\n", __func__);
 }
 
 static int rcm_pl060_probe(struct platform_device *pdev)
@@ -300,22 +240,24 @@ static int rcm_pl060_probe(struct platform_device *pdev)
 	const struct of_device_id *match;
 	void __iomem *base;
 	struct resource *res;
-	int cnt_irqs;
-//	struct irq_chip_generic *gc;
+	struct irq_chip_generic *gc;
 	int i;
-//	int err;
+	int err;
 
 	match = of_match_device(rcm_pl060_of_match, dev);
 	if (!match)
 		return -EINVAL;
-
-	cnt_irqs = of_irq_count(np);
 
 	data = devm_kzalloc(dev, sizeof(struct rcm_pl060_chip), GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
 
 	platform_set_drvdata(pdev, data);
+
+	data->cnt_irqs = of_irq_count(np);
+
+	if (data->cnt_irqs > RCM_PL060_GPIO_NR)
+		data->cnt_irqs = RCM_PL060_GPIO_NR;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	base = devm_ioremap_resource(dev, res);
@@ -349,13 +291,9 @@ static int rcm_pl060_probe(struct platform_device *pdev)
 	data->chip.direction_output = rcm_pl060_direction_output;
 	data->chip.get              = rcm_pl060_get_value;
 	data->chip.set              = rcm_pl060_set_value;
-/*
-	if (cnt_irqs)
-		data->chip.to_irq   = rcm_pl060_to_irq;
-*/
-	if (cnt_irqs) {
-/*
-		data->domain = irq_domain_add_linear(np, RCM_PL060_GPIO_NR,
+
+	if (data->cnt_irqs) {
+		data->domain = irq_domain_add_linear(np, data->cnt_irqs,
 		                                     &irq_generic_chip_ops,
 		                                     NULL);
 		if (!data->domain) {
@@ -365,12 +303,12 @@ static int rcm_pl060_probe(struct platform_device *pdev)
 		}
 
 		err = irq_alloc_domain_generic_chips(
-		    data->domain, RCM_PL060_GPIO_NR, 2, np->name,
+		    data->domain, data->cnt_irqs, 2, np->name,
 		    handle_level_irq, IRQ_NOREQUEST | IRQ_NOPROBE | IRQ_LEVEL,
 		    0, 0);
 		if (err) {
 			dev_err(dev, "couldn't allocate irq chips %s (DT).\n",
-				data->chip.label);
+			        data->chip.label);
 			goto err_domain;
 		}
 
@@ -393,7 +331,7 @@ static int rcm_pl060_probe(struct platform_device *pdev)
 			gc->chip_types[i].chip.name = data->chip.label;
 		}
 
-		for (i = 0; i < RCM_PL060_GPIO_NR; i++) {
+		for (i = 0; i < data->cnt_irqs; i++) {
 			int irq = platform_get_irq(pdev, i);
 
 			if (irq < 0)
@@ -402,63 +340,9 @@ static int rcm_pl060_probe(struct platform_device *pdev)
 			                                 rcm_pl060_irq_handler,
 			                                 data);
 		}
-*/
 
-		struct device_node *irq_parent;
-		struct gpio_irq_chip *irq;
-
-		if (cnt_irqs < RCM_PL060_GPIO_NR) {
-			dev_warn(dev, "Count interrupts (%d IRQs) does not "
-			              "match to GPIO count (%d GPIOs).\n",
-			              cnt_irqs, data->chip.ngpio);
-		}
-
-		if (cnt_irqs > RCM_PL060_GPIO_NR)
-			cnt_irqs = RCM_PL060_GPIO_NR;
-
-		data->irqs = devm_kcalloc(dev, cnt_irqs, sizeof(*data->irqs),
-		                          GFP_KERNEL);
-		if (!data->irqs)
-			return -ENOMEM;
-
-		for (i = 0; i < cnt_irqs; i++) {
-			int irq = platform_get_irq(pdev, i);
-			if (irq < 0)
-				return irq;
-pr_debug("%s: irq[%d] = %d\n", __func__, i, irq);
-			data->irqs[i] = irq;
-		}
-
-		data->intc.name = data->chip.label;
-		data->intc.irq_ack      = rcm_pl060_irq_ack;
-		data->intc.irq_mask     = rcm_pl060_irq_mask;
-		data->intc.irq_unmask   = rcm_pl060_irq_unmask;
-		data->intc.irq_set_type = rcm_pl060_irq_set_type;
-
-		irq = &data->chip.irq;
-		irq->chip = &data->intc;
-		irq->fwnode = of_node_to_fwnode(np);
-pr_debug("%s: fwnode = 0x%X\n", __func__, (u32)irq->fwnode);
-		irq->child_to_parent_hwirq  = rcm_pl060_child_to_parent_hwirq;
-		irq->populate_parent_fwspec = rcm_pl060_populate_parent_fwspec;
-		irq->handler = handle_simple_irq;
-		irq->default_type = IRQ_TYPE_NONE;
-		irq->parent_handler = rcm_pl060_irq_handler;
-		irq->parent_handler_data = data;
-		irq->num_parents = cnt_irqs;
-		irq->parents = data->irqs;
-
-		irq_parent = of_irq_find_parent(np);
-		if (!irq_parent) {
-			dev_err(dev, "no IRQ parent node\n");
-			return -ENODEV;
-		}
-
-		irq->parent_domain = irq_find_host(irq_parent);
-		if (!irq->parent_domain) {
-			dev_err(dev, "no IRQ parent domain\n");
-			return -ENODEV;
-		}
+		for (i = 0; i < data->cnt_irqs; ++i)
+			irq_create_mapping(data->domain, i);
 	}
 
 	devm_gpiochip_add_data(dev, &data->chip, data);
@@ -466,12 +350,25 @@ pr_debug("%s: fwnode = 0x%X\n", __func__, (u32)irq->fwnode);
 	dev_info(dev, "initialized\n");
 
 	return 0;
-/*
+
 err_domain:
 	irq_domain_remove(data->domain);
 
 	return err;
-*/
+}
+
+static int rcm_pl060_remove(struct platform_device *pdev)
+{
+	struct rcm_pl060_chip *data = platform_get_drvdata(pdev);
+	int i;
+
+	if (data->domain) {
+		for (i = 0; i < data->cnt_irqs; ++i)
+			irq_dispose_mapping(irq_find_mapping(data->domain, i));
+		irq_domain_remove(data->domain);
+	}
+
+	return 0;
 }
 
 static const struct of_device_id rcm_pl060_of_match[] = {
@@ -487,5 +384,6 @@ static struct platform_driver rcm_pl060_driver = {
 		.of_match_table = rcm_pl060_of_match,
 	},
 	.probe = rcm_pl060_probe,
+	.remove = rcm_pl060_remove
 };
 builtin_platform_driver(rcm_pl060_driver);
